66c7bd9ea538c462e3782f83fc0535fd6cc58763
iam-ismaeel <kasaliismail2@gmail.com>
2026-01-06 16:03:20 +0100

    modified the variable.tf files

--- terraform-modules/controller/5-variables.tf
+++ terraform-modules/controller/5-variables.tf
@@ -34,3 +34,25 @@ variable "cluster_certificate_authority" {
 variable "vpc_id" {
   type = string
 }
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  token                  = data.aws_eks_cluster_auth.this.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    token                  = data.aws_eks_cluster_auth.this.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  }
+}
--- terraform-modules/kubernetes-addons/7-variables.tf
+++ terraform-modules/kubernetes-addons/7-variables.tf
@@ -48,3 +48,25 @@ variable "cluster_auth_token" {
 variable "cluster_certificate_authority" {
   type = string
 }
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  token                  = data.aws_eks_cluster_auth.this.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    token                  = data.aws_eks_cluster_auth.this.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  }
+}
